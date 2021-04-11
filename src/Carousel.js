5fb0822ecde1a4800d89aab7ef0366e2767bad9f
Wojtek Rupik <w.rupik@selleo.com>
2021-04-11 19:47:13 +0200

    Prev and Next handler added

--- src/Carousel.js
+++ src/Carousel.js
@@ -22,14 +22,28 @@ const Carousel = ({ delay, children }) => {
     const cleanup = () => clearInterval(intervalId);
 
     return cleanup;
-  });
+  }, [slideIndex]);
+
+  const prevHandler = () => {
+    setSlideIndex((currentIndex) => {
+      if (currentIndex === 0) return children.length - 1;
+      return currentIndex - 1;
+    });
+  };
+
+  const nextHandler = () => {
+    setSlideIndex((currentIndex) => {
+      if (currentIndex === children.length - 1) return 0;
+      return currentIndex + 1;
+    });
+  };
 
   return (
     <div className="carousel">
       <div>{children[slideIndex]}</div>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={() => prevHandler()}>Previous</button>
+        <button onClick={() => nextHandler()}>Next</button>
       </div>
     </div>
   );
